41a99b8da9ebe184c1192f3d32aff17fb91d19c2
Kaitian Xie <xkaitian@gmail.com>
2018-08-28 19:16:34 -0700

    Go - Hello World: Finish problem

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,11 @@
+ /*
+Package greeting implements a simple hello world function.
+
+ */
+package greeting
+
+// HelloWorld return a string "Hello, World!".
+func HelloWorld() string {
+
+	return "Hello, World!"
+}
