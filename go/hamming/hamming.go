e44863f2eab1cff1fb0bdcb64240ac1d744a2f32
Kaitian Xie <xkaitian@gmail.com>
2018-08-30 03:27:43 -0700

    Go - Hamming: Finish problem

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,22 @@
+/*
+Package hamming calculates the Hamming difference between two DNA strands.
+*/
 package hamming
 
+import "github.com/pkg/errors"
+
+// Distance calculates the Hamming difference between two DNA strands. It returns an error if the two strands do not
+// have the same length
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("two dna strands must have the same length")
+	}
+
+	count := 0
+	for i := range a {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count, nil
 }
